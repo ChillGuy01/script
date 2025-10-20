15cddec94927cb0044648e24642e1356d0658ac5
ChillGuy01 <142322781+ChillGuy01@users.noreply.github.com>
2025-10-20 17:50:15 +0700

    Update sus.lua

--- sus/sus.lua
+++ sus/sus.lua
@@ -103,7 +103,7 @@ getgenv().ExunysDeveloperAimbot = {
 		OffsetToMoveDirection = false,
 		OffsetIncrement = 15,
 
-		Sensitivity = 0.09, -- Animation length (in seconds) before fully locking onto target
+		Sensitivity = 0.07, -- Animation length (in seconds) before fully locking onto target
 		Sensitivity2 = 3.5, -- mousemoverel Sensitivity
 
 		LockMode = 2, -- 1 = CFrame; 2 = mousemoverel
